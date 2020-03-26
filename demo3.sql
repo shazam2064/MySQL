d2cd915111f03ca62d75d85121a04056683a89bc
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2020-03-26 15:56:39 -0400

    Update demo3.sql

--- demo3.sql
+++ demo3.sql
@@ -1,4 +1,4 @@
-USE CONTACTS; 
+USE CONTACTS;
 
 /* SELECT p.person_first_name, person_last_name
 FROM person p
@@ -18,9 +18,21 @@ WHERE p.person_last_name = 'Ahern';
  FROM person p;
  
   SELECT SUM(p.person_contacted_number)
+ FROM person p; */ 
+ 
+ /* SELECT COUNT(DISTINCT p.person_first_name)
  FROM person p; */
- 
-  SELECT COUNT(DISTINCT p.person_first_name)
- FROM person p;
- 
+
+/* SELECT 
+COUNT(p.person_first_name),
+p.person_first_name
+FROM person p
+GROUP BY p.person_first_name;*/
+
+SELECT 
+COUNT(p.person_first_name) as FirstNameCount,
+p.person_first_name
+FROM person p
+GROUP BY p.person_first_name
+HAVING FirstNameCount > 0;
  
