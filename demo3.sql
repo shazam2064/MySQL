9d15c30014b0ceb40634c30d76e1e733ddb32f09
gabriel <gabriel@mac-mini-2.hsd1.ga.comcast.net>
2020-03-26 10:48:30 -0400

    updated demo3

--- demo3.sql
+++ demo3.sql
@@ -1,5 +1,22 @@
 USE CONTACTS; 
 
-SELECT p.person_first_name, person_last_name
+/* SELECT p.person_first_name, person_last_name
 FROM person p
-ORDER BY p.person_last_name; 
+ORDER BY p.person_last_name;*/
+
+ SELECT COUNT(person_first_name)
+FROM person p
+WHERE p.person_last_name = 'Ahern';
+
+ SELECT MAX(p.person_contacted_number)
+ FROM person p;
+ 
+  SELECT MIN(p.person_contacted_number)
+ FROM person p;
+ 
+  SELECT AVG(p.person_contacted_number)
+ FROM person p;
+ 
+  SELECT SUM(p.person_contacted_number)
+ FROM person p;
+ 
